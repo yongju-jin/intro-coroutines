19cb5e15c37e05fc5f3b55b5420c6496ef567c1b
yongju.jin <yong8521@gmail.com>
2019-12-25 18:55:08 +0900

    :art: Add Callback

--- src/tasks/Request2Background.kt
+++ src/tasks/Request2Background.kt
@@ -7,6 +7,7 @@ import kotlin.concurrent.thread
 
 fun loadContributorsBackground(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     thread {
-        loadContributorsBlocking(service, req)
+        val result = loadContributorsBlocking(service, req)
+        updateResults(result)
     }
 }
