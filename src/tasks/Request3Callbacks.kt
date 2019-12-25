66581e7c865e996b2a1ba88f5504e4f4e8a7edf9
yongju.jin <yong8521@gmail.com>
2019-12-25 19:10:23 +0900

    :art: Add callback

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -5,6 +5,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
 
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
@@ -12,14 +13,17 @@ fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateRe
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
         val allUsers = mutableListOf<User>()
+        val countDownLatch = CountDownLatch(repos.size)
         for (repo in repos) {
             service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
                 allUsers += users
+                countDownLatch.countDown()
             }
         }
         // TODO: Why this code doesn't work? How to fix that?
+        countDownLatch.await()
         updateResults(allUsers.aggregate())
     }
 }
